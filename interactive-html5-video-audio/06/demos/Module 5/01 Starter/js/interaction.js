ef92edc1e6f5b4a9974e8ce65f8071d26e3eafdc
Pariente <neodante12@gmail.com>
2025-01-09 17:19:20 -0700

    toca reparar porque no funciona el cambio de album cuando cambia la variable

--- interactive-html5-video-audio/06/demos/Module 5/01 Starter/js/interaction.js
+++ interactive-html5-video-audio/06/demos/Module 5/01 Starter/js/interaction.js
@@ -1,7 +1,55 @@
 //Variables
 
-var audiofile = document.getElementById('audioFile');
+var audioFile = document.getElementById('audioFile');
+var coverImg = document.getElementById('coverImg');
+var bandNAme = document.getElementById('bandName');
+var songNAme = document.getElementById('songName');
 
-audiofile.onloadedmetadata = function(){
-    
+var currentAlbum = 0;
+
+//json
+var albumList = {
+    "albums": [
+        {"song":"media/Audio_1.mp3", "albumCover":"media/album1.png","bandNAme": "Band name 1", "songNAme": "The Famouse song"},
+        {"song":"media/Audio_2.mp3", "albumCover":"media/album2.png","bandNAme": "Band name 2", "songNAme": "The other Famouse song"},
+    ]
+}
+
+//load
+audioFile.onloadedmetadata = function(){
+    loadAlbum(currentAlbum);
+}
+
+function loadAlbum(e){
+    coverImg.src = albumList.albums[e].albumCover;
+    bandNAme.innerHTML = albumList.albums[e].bandNAme;
+    songNAme.innerHTML = albumList.albums[e].songNAme;
+}
+
+//back
+
+function backAudio(){
+   
+}
+
+// Play
+
+function playAudio(){
+    if (audioFile.paused){
+        audioFile.play();
+    } else {
+        audioFile.pause();
+    }
+}
+
+// Next
+
+function nextAudio(){
+ 
+}
+
+// Ended
+
+audioFile.onended = function(){
+    alert('audio has ended')
 }
