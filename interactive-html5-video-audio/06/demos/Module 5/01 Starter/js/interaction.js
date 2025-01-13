993e7acd3a00019bffe0e644db07ef9fb5c6c6f7
Pariente <neodante12@gmail.com>
2025-01-13 15:07:00 -0700

    en este momento funciona

--- interactive-html5-video-audio/06/demos/Module 5/01 Starter/js/interaction.js
+++ interactive-html5-video-audio/06/demos/Module 5/01 Starter/js/interaction.js
@@ -10,20 +10,20 @@ var currentAlbum = 0;
 //json
 var albumList = {
     "albums": [
-        {"song":"media/Audio_1.mp3", "albumCover":"media/album1.png","bandNAme": "Band name 1", "songNAme": "The Famouse song"},
-        {"song":"media/Audio_2.mp3", "albumCover":"media/album2.png","bandNAme": "Band name 2", "songNAme": "The other Famouse song"},
+        {"song":"media/Audio_1.mp3", "albumCover":"media/album1.png","bandName": "Band name 1", "songName": "The Famouse song"},
+        {"song":"media/Audio_2.mp3", "albumCover":"media/album2.png","bandName": "Band name 2", "songName": "The other Famouse song"},
     ]
 }
 
 //load
-audioFile.onloadedmetadata = function(){
-    loadAlbum(currentAlbum);
-}
+loadAlbum(currentAlbum);
+
+
 
 function loadAlbum(e){
     coverImg.src = albumList.albums[e].albumCover;
-    bandNAme.innerHTML = albumList.albums[e].bandNAme;
-    songNAme.innerHTML = albumList.albums[e].songNAme;
+    bandNAme.innerHTML = albumList.albums[e].bandName;
+    songNAme.innerHTML = albumList.albums[e].songName;
 }
 
 //back
